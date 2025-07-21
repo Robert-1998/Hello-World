39fe5ec2895d9d7f366ceb4daf997078c6fa40bd
Daniyar <daniyar.khusainov777@gmail.com>
2025-07-21 17:01:40 +0300

    Есть хилка

--- project_1.c
+++ project_1.c
@@ -5,14 +5,17 @@
 
 #define MAX_BULLETS 100
 #define MAX_DOGS 100
-
-int health = 5;
+#define MAX_COUNT_HEALTH 5
+int health_ind = 5;
 
 void drawing(int y, int x);
 void control(int *x, int *y, int key, int *dx, int *dy);
 void spawn_dogs();
+void spawn_car();
+void spawn_health();
 void move_dogs(int player_x, int player_y);
 void check_collision(int player_x, int player_y);
+void check_health_collision(int player_x, int player_y);
 void check_bullet_collision();
 
 typedef struct {
@@ -31,6 +34,24 @@ typedef struct {
   int status;
 } Dog;
 
+typedef struct {
+  int x, y;
+  int status;
+} Car;
+
+typedef struct {
+  int x, y;
+  int status;
+} Health;
+
+
+Health health;
+int health_count = 0;
+
+int in_car = 0;
+int car_speed = 2;  
+
+Car car;
 Dog dogs[MAX_DOGS];
 int dog_count = 0;
 
@@ -40,12 +61,16 @@ int main() {
     init_pair(1, COLOR_MAGENTA, COLOR_BLACK);
     init_pair(2, COLOR_GREEN, COLOR_BLACK);
     init_pair(3, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(4, COLOR_WHITE, COLOR_BLACK);
+    init_pair(5, COLOR_BLACK, COLOR_WHITE);
     nodelay(stdscr, TRUE);
     noecho();
     cbreak();
     keypad(stdscr, TRUE);
     srand(time(NULL));
+    int car_spawn_timer = 0;
     int dog_spawn_timer = 0;
+    int health_spawn_timer = 0;
     int x = 0, y = 0;
     int dx = 1, dy = 0;
     int key;
@@ -67,15 +92,28 @@ int main() {
                 }
             }
         }
+        
+        car_spawn_timer++;
+        if (car_spawn_timer >= 100 && !car.status) {
+            spawn_car();
+            car_spawn_timer = 0;
+        }
 
         dog_spawn_timer++;
         if(dog_spawn_timer >= 50) {
           spawn_dogs();
           dog_spawn_timer = 0;
         }
+
+        health_spawn_timer++;
+        if (health_spawn_timer >= 70) {
+            spawn_health();
+            health_spawn_timer = 0;
+        }
         
         move_dogs(x, y);
         check_collision(x, y);
+        check_health_collision(x, y);
         check_bullet_collision();
 
         drawing(y, x);
@@ -108,31 +146,44 @@ void drawing(int y, int x) {
           attroff(COLOR_PAIR(3));
       }
     }
-    mvprintw(0, 0, "Health: %d", health);
+
+  if (car.status) {
+    attron(COLOR_PAIR(4));
+    mvprintw(car.y, car.x, "[[[]]]");
+    attroff(COLOR_PAIR(4));
+  }
+
+  if (health.status) {
+    attron(COLOR_PAIR(5));
+    mvprintw(health.y, health.x, "+");
+    attroff(COLOR_PAIR(5));
+  }
+    mvprintw(0, 0, "Health: %d", health_ind);
     refresh();
 }
 
 
 void control(int *x, int *y, int key, int *dx, int *dy) {
+  int speed = in_car ? car_speed : 1;
   switch(key) {
 
     case KEY_UP:
-      (*y)--;
+      (*y) -= speed;
       *dx = 0, *dy = -1;
       break;
 
     case KEY_DOWN:
-      (*y)++;
+      (*y) += speed;
       *dx = 0, *dy = +1;
       break;
 
     case KEY_LEFT:
-      (*x)--;
+      (*x) -= speed;
       *dx = -1, *dy = 0;
       break;
 
     case KEY_RIGHT:
-      (*x)++;
+      (*x) += speed;
       *dx = +1, *dy = 0;
       break;
 
@@ -147,6 +198,15 @@ void control(int *x, int *y, int key, int *dx, int *dy) {
       }
     break;
 
+    case 'z':
+    if (car.status &&
+        abs(*x - car.x) <= 1 &&
+        abs(*y - car.y) <= 1) {
+        in_car = 1;
+        car.status = 1; 
+    }
+    break;
+
   }
 
 
@@ -158,6 +218,29 @@ void control(int *x, int *y, int key, int *dx, int *dy) {
   if (*y < 0) *y = 0;
   if (*y >= max_y) *y = max_y - 1;
 
+}
+
+void spawn_car() {
+    if (!car.status) {
+        int max_y, max_x;
+        getmaxyx(stdscr, max_y, max_x);
+
+        car.x = rand() % max_x;
+        car.y = rand() % max_y;
+        car.status = 1;
+    }
+}
+
+void spawn_health() {
+    if (!health.status && health_count < MAX_COUNT_HEALTH) {
+        int max_y, max_x;
+        getmaxyx(stdscr, max_y, max_x);
+
+        health.x = rand() % max_x;
+        health.y = rand() % max_y;
+        health.status = 1;
+        health_count++;
+    }
 }
 
 void spawn_dogs() {
@@ -198,12 +281,21 @@ void move_dogs(int player_x, int player_y) {
 
 void check_collision(int player_x, int player_y) {
   for(int i = 0; i < dog_count; i++) {
-       if(dogs[i].status && dogs[i].x == player_x && dogs[i].y == player_y) {
-          health--;
-          dogs[i].status = 0;
-       }
+    if(dogs[i].status && dogs[i].x == player_x && dogs[i].y == player_y) {
+      health_ind--;
+      dogs[i].status = 0;
+    }
   }
 }
+
+void check_health_collision(int player_x, int player_y) {
+    if (health.status && health.x == player_x && health.y == player_y) {
+        health_ind++;
+        health.status = 0;
+        health_count = 0;
+    }
+}
+
 
 void check_bullet_collision() {
   for(int i = 0; i < bullet_count; i++) {
